e03218b48de73feb1cfcdeaeb2f04db8b6ada8c4
Devulpally-Sindhu <sindhu09.dev@gmail.com>
2024-09-30 00:01:04 +0530

    changes-on-homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    component:HomeComponent
+  },
+  {
+    path:"contact",
+    component:ContactusComponent
+  },
+  {
+    path:"aboutus",
+    component:AboutusComponent
+  },
+  {
+    path:"",
+    component:  HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ContactusComponent,
+    AboutusComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
